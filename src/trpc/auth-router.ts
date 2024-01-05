006385d5f7f131bbbb150ed72be28d4596be8a6e
Bermuda <olasunkanmiboluwatife110@gmail.com>
2024-01-05 15:00:02 +0100

    (feat): sign in logic

--- src/trpc/auth-router.ts
+++ src/trpc/auth-router.ts
@@ -55,4 +55,30 @@ export const authRouter = router({
 
       return { success: true }
     }),
+
+
+    // sign in api endpoint
+  signIn: publicProcedure
+    .input(AuthCredentialsValidator)
+    .mutation(async ({ input, ctx }) => {
+      const { email, password } = input
+      const { res } = ctx
+
+      const payload = await getPayloadClient()
+
+      try {
+        await payload.login({
+          collection: 'users',
+          data: {
+            email,
+            password,
+          },
+          res,
+        })
+
+        return { succes: true }
+      } catch (err) {
+        throw new TRPCError({ code: 'UNAUTHORIZED' })
+      }
+    }),
 })
