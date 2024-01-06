252f32bef50eda860c44c8047ff5bbc1274cbe0f
Bermuda <olasunkanmiboluwatife110@gmail.com>
2024-01-06 12:02:58 +0100

    (feat): security on Media collection

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -1,4 +1,21 @@
-import { CollectionConfig } from 'payload/types'
+import { User } from '../payload-types'
+import { Access, CollectionConfig } from 'payload/types'
+
+// can you read images or not
+const isAdmin =
+  (): Access =>
+  async ({ req }) => {
+    const user = req.user as User | undefined // this would be undefined if user is not logged in
+
+    if (!user) return false
+    if (user.role === 'admin') return true
+
+    return {
+      user: {
+        equals: req.user.id, // if the image field === the logged in user
+      },
+    }
+  }
 
 export const Media: CollectionConfig = {
   slug: 'media',
@@ -8,6 +25,20 @@ export const Media: CollectionConfig = {
         return { ...data, user: req.user.id }
       },
     ],
+  },
+  access: {
+    read: async ({ req }) => {
+      const referer = req.headers.referer
+
+      // if users are not logged in
+      if (!req.user || !referer?.includes('admin')) {
+        return true
+      }
+
+      return await isAdmin()({ req }) //
+    },
+    delete: isAdmin(),
+    update: isAdmin(),
   },
   admin: {
     hidden: ({ user }) => user.role !== 'admin',
